d24a9134409885fbb46e8802d292e5085c3184af
Aleksejs Gordejevs <aleksej@gordejev.lv>
2026-02-05 13:02:52 +0200

    Update Next.js configuration to use `standalone` output and add `NextConfig` type annotation

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Config options here
+const nextConfig: NextConfig = {
+  output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
